d2da681daacb4604d288219e292c28376031057c
Alberto Gómez <alberto.grodriguez@bosonit.com>
2023-10-26 10:59:05 +0200

    Seccion 21. Mapas en Angular terminado

--- src/app/maps/components/mini-map/mini-map.component.ts
+++ src/app/maps/components/mini-map/mini-map.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, Input, ViewChild } from '@angular/core';
+import { Map, Marker } from 'mapbox-gl';
 
 @Component({
-  selector: 'app-mini-map',
+  selector: 'map-mini-map',
   templateUrl: './mini-map.component.html',
-  styleUrls: ['./mini-map.component.css']
+  styleUrls: ['./mini-map.component.css'],
 })
 export class MiniMapComponent {
+  @Input() lngLat?: [number, number];
+  @ViewChild('map') divMap?: ElementRef;
 
-}
+  ngAfterViewInit() {
+    if(!this.divMap?.nativeElement) throw 'LngLat no encontrado';
+    if (!this.lngLat) throw 'LngLat no encontrado';
+
+    //mapa
+
+    const map = new Map({
+      container: this.divMap?.nativeElement,
+      style: 'mapbox://styles/mapbox/streets-v12',
+      center: this.lngLat,
+      zoom: 15,
+    });
+    new Marker()
+    .setLngLat(this.lngLat)
+    .addTo(map)
+  }
+
+
+  }
